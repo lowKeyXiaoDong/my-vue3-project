1f5c25247b536bc0b3f6c88755f9707381678b35
hanxiaodong <xiaodong.han@fenbeitong.com>
2021-11-16 19:06:12 +0800

    [1116]:[feat](全局):  todoList.vue

---
+++ src/page/home.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>这是首页</h1>
+  <TodoList />
+  <h1 @click="add">{{ count }}</h1>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import TodoList from '../components/TodoList.vue'
+
+let count = ref(0)
+let color = ref('red')
+
+function add() {
+  count.value++
+  color.value = Math.random() > 0.5 ? 'blue' : 'red'
+}
+</script>
+
+<style scoped>
+h1 {
+  color: v-bind(color);
+}
+</style>
