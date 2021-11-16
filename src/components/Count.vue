09108d695d42ce43e1f4e2c270483b679de7928f
aibaoxiaodon <sxczhxd@163.com>
2021-11-16 22:32:20 +0800

    ✨ feat(1116): 全局
    自定义路由和store

---
+++ src/components/Count.vue
@@ -0,0 +1,20 @@
+<template>
+  <div @click="add">
+    {{ count }}
+
+    {{ double }}
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+let store = useStore()
+let count = computed(() => store.state.count)
+let double = computed(() => store.getters.double)
+
+function add() {
+  // store.commit('add')
+  store.dispatch('asyncAdd')
+}
+</script>
--- src/page/home.vue
+++ src/page/home.vue
@@ -2,11 +2,13 @@
   <h1>这是首页</h1>
   <TodoList />
   <h1 @click="add">{{ count }}</h1>
+  <Count />
 </template>
 
 <script setup>
 import { ref } from 'vue'
 import TodoList from '../components/TodoList.vue'
+import Count from '../components/Count.vue'
 
 let count = ref(0)
 let color = ref('red')
