5730f48439fe2f58e7095e306bf75cc7c6250ce0
aibaoxiaodon <sxczhxd@163.com>
2022-03-10 22:17:20 +0800

    测试

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <router-link to="/">首页</router-link>
-    <router-link to="/about">关于</router-link>
+    <router-link to="/about">关于123</router-link>
   </div>
   <router-view></router-view>
 </template>
